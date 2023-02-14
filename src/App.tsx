eb41fde0cdb678fc13c56f952f71d77deb3890bb
Yan Skvarada <yan@MacBook-Air-Yan.local>
2023-02-14 15:00:31 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,18 @@ import React from 'react';
 import './App.css';
 
 import Title from './components/Title';
-import UserMenu from "./components/UserMenu"
+import UserMenu from "./components/UserMenu";
+import Button from "./components/Buttom"
+import { ButtonType } from "./components/Buttom/Button";
 
-function App() {
+const App = () => {
   return (
         <div>
+
+          <Button type={ButtonType.Primary} title={`Primary`} onClick={()=> {}}/>
+          <Button type={ButtonType.Secondary} title={`Secondary`} onClick={()=> {}}/>
+          <Button type={ButtonType.Errer} title={`Errer`} onClick={()=> {}}/>
+
           <UserMenu nameUser={`Bob Lapuch`}/>
           <Title title={`Blog`} />
 
