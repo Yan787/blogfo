a6fa6fd8a20191f62e3b357f83b6d3fd4af41985
Yan Skvarada <yan@MacBook-Air-Yan.local>
2023-03-09 02:08:09 +0300

    init

--- src/pages/RegConfirmation/RegConfirmation.tsx
+++ src/pages/RegConfirmation/RegConfirmation.tsx
@@ -4,14 +4,21 @@ import Frame from "../../components/Frame";
 import FormPage from "../FormPage/FormProps";
 import { ButtonType } from "../../components/Buttom/Button";
 import styles from "./RegConfirmation.module.scss";
+import classNames from "classnames";
+import { Theme, useThemeContext } from "../../context/Theme/Context";
 
 const RegConfirmation = () => {
+  const { theme } = useThemeContext();
   return (
     <div>
       <FormPage title={"Registration Confirmation"} />
       <Frame>
         <div className={styles.container}>
-          <div>
+          <div
+            className={classNames(styles.text, {
+              [styles.darkText]: theme === Theme.Dark,
+            })}
+          >
             Please activate your account with the activation link in the email
             .Please, check your email
           </div>
